02de95a94c1aa34b17db2168dfe6cd9384816dbc
Ami-tnk <ami1y251992@gmail.com>
2020-12-12 07:07:38 +0000

    [Update]新規投稿画面

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,22 @@
 class TasksController < ApplicationController
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path, success:"タスク「#{@task.name}」を登録しました。"
+  end
+
+  def index
+    @tasks = Task.all
+  end
+
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :discription)
+  end
 end
