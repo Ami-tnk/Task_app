4dff67c8359eaa62852fccee06a6371781331994
Ami-tnk <ami1y251992@gmail.com>
2020-12-14 05:29:28 +0000

    [Add]User情報, 詳細画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
-  def new
-    @usrer = User
+  def show
+    @user = User.find(params[:id])
   end
-  
-  def create
+
+  def edit
+    @user = User.find(params[:id])
   end
-  
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
+  resources :users, only: [:show, :edit, :update]
 
-  root to: 'tasks#index'
+  root to: 'homes#top'
   resources :tasks
 end
