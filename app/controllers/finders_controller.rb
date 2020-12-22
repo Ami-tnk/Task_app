b347f2f9a3666851f17de90fbd92730788ab8a83
Ami-tnk <ami1y251992@gmail.com>
2020-12-22 09:55:50 +0000

    [Finish]検索機能の追加

--- app/controllers/finders_controller.rb
+++ app/controllers/finders_controller.rb
@@ -1,4 +1,34 @@
 class FindersController < ApplicationController
   def finder
+    @model = params["finder"]["model"]
+    @value = params["finder"]["value"]
+    @how = params["finder"]["how"]
+    @datas = finder_for(@model, @value, @how)
   end
+
+  def match(model, value)
+    if model == 'user'
+      User.where(name: value)
+    elsif model == 'task'
+      Task.where(name: value)
+    end
+  end
+
+  def partial(model, value)
+    if model == 'user'
+      User.where("name LIKE?", "%#{value}%")
+    elsif model == 'task'
+      Task.where("name LIKE?", "%#{value}%")
+    end
+  end
+
+  def finder_for(model, value, how)
+    case how
+    when 'match'
+      match(model,value)
+    when 'partial'
+      partial(model,value)
+    end
+  end
+
 end
